2a64bec2d029fef0d60f196ba0df48273b2ee201
uaggit99 <ace77ey@gmail.com>
2022-11-04 18:11:42 -0500

    creacion de modelos, relaciones, repositorios y controlador

---
+++ src/models/usuario.model.ts
@@ -0,0 +1,84 @@
+import {Entity, model, property, belongsTo} from '@loopback/repository';
+import {Parque} from './parque.model';
+
+@model()
+export class Usuario extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  fecha: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombres: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  apellidos: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  documento: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  correo: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  cargo: string;
+
+  @property({
+    type: 'string',
+  })
+  telefonofijo?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  celular: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  clave: string;
+
+  @property({
+    type: 'string',
+    required: false,
+  })
+  id_parque: string;
+
+  @belongsTo(() => Parque)
+  parqueId: string;
+
+  constructor(data?: Partial<Usuario>) {
+    super(data);
+  }
+}
+
+export interface UsuarioRelations {
+  // describe navigational properties here
+}
+
+export type UsuarioWithRelations = Usuario & UsuarioRelations;
